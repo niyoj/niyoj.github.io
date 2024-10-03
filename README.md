074a73a362ba48d42c57f7570ecb0822845f44ab
Niyoj Oli <oliniyoj@gmail.com>
2024-10-03 21:28:57 +0545

    makes file structure in pm way not react way

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Starting point of portfolio website
+
+# References
+- [https://profy.dev/article/react-folder-structure#world-domination-colocation](Folder Structure for React Projects)
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./app.jsx";
 
-createRoot(document.getElementById('root')).render(
+// importing necessary css stylesheets
+import "./assets/stylesheets/reset.css";
+import "./assets/stylesheets/variables.css";
+import "./assets/stylesheets/utilities.css";
+
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
