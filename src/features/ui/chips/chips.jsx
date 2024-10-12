ec36ecf5ad1080f179f51401238004dcab83ae77
Niyoj Oli <oliniyoj@gmail.com>
2024-10-12 17:14:26 +0545

    creates chips for filtering

---
+++ src/features/ui/chips/chips.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+import Check from "@assets/images/icons/check.svg?react";
+
+import styles from "./chips.module.css";
+
+export function Chips({ children }) {
+  const [checked, setChecked] = useState(false);
+
+  const handleChipsClicked = () => {
+    setChecked((prev) => !prev);
+  };
+
+  return (
+    <div
+      className={`${styles["chips"]} ${checked ? styles["chips--active"] : styles["chips--inactive"]}`}
+    >
+      <label>
+        <input type="checkbox" onChange={handleChipsClicked} />
+        {checked && <Check />}
+        <small>{children}</small>
+      </label>
+    </div>
+  );
+}
+
+Chips.propTypes = {
+  children: PropTypes.node.isRequired,
+};
--- src/features/ui/index.js
+++ src/features/ui/index.js
@@ -2,3 +2,4 @@ export { Button } from "./button/button.jsx";
 export { Logo } from "./logo/logo.jsx";
 export { useDelay } from "./use-delay/use-delay.jsx";
 export { ScrollHint } from "./scroll-hint/scroll-hint.jsx";
+export { Chips } from "./chips/chips.jsx";
