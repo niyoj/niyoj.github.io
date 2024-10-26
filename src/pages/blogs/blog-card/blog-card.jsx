4bbd85651b000a12c6490a7e72a5d73c4626c79e
Niyoj Oli <oliniyoj@gmail.com>
2024-10-26 22:59:43 +0545

    completes blog section

--- src/pages/blogs/blog-card/blog-card.jsx
+++ src/pages/blogs/blog-card/blog-card.jsx
@@ -2,7 +2,26 @@ import PropTypes from "prop-types";
 import styles from "./blog-card.module.css";
 
 export function BlogCard({ title, description, publishedDate, readTime }) {
-  return <p>blog</p>;
+  // in DD Mon YYYY format
+  const dateString = new Date(publishedDate).toLocaleString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+  });
+
+  return (
+    <div className={styles["blog"]}>
+      <div className={styles["blog__meta"]}>
+        <p>{readTime} Mins Read</p>
+        <small>{dateString}</small>
+      </div>
+
+      <div className={styles["blog__info"]}>
+        <h3>{title}</h3>
+        <small>{description}</small>
+      </div>
+    </div>
+  );
 }
 
 BlogCard.propTypes = {
--- src/pages/blogs/blogs.jsx
+++ src/pages/blogs/blogs.jsx
@@ -8,7 +8,7 @@ const dummyBlog = {
   publishedDate: "2020 december 12",
   title: "What are portals in React? A practical guide",
   description:
-    "Not only can Doctor Strange in the MCU create portals, but as a React developer, so can you! The only difference is that while Doctor Strange's portals let people travel across dimensions, your portals let components move across the DOM. This blog is a mighty scroll that helps you master the React portals so that you can teleport those React components on your own.",
+    "Not only can Doctor Strange in the MCU create portals, but as a React developer, so can you! This blog is a mighty scroll that helps you master the React portals so that you can teleport those React components on your own.",
 };
 
 export function Blogs() {
@@ -20,12 +20,32 @@ export function Blogs() {
         description="I enjoy sharing what I've learned, collecting insights and experiences into blogs that I share to help others explore new ideas and skills."
       />
 
-      <BlogCard
-        readTime={dummyBlog["readTime"]}
-        publishedDate={dummyBlog["publishedDate"]}
-        title={dummyBlog["title"]}
-        description={dummyBlog["descrtiption"]}
-      />
+      <div className={styles["blogs__display"]}>
+        <BlogCard
+          readTime={dummyBlog["readTime"]}
+          publishedDate={dummyBlog["publishedDate"]}
+          title={dummyBlog["title"]}
+          description={dummyBlog["description"]}
+        />
+        <BlogCard
+          readTime={dummyBlog["readTime"]}
+          publishedDate={dummyBlog["publishedDate"]}
+          title={dummyBlog["title"]}
+          description={dummyBlog["description"]}
+        />
+        <BlogCard
+          readTime={dummyBlog["readTime"]}
+          publishedDate={dummyBlog["publishedDate"]}
+          title={dummyBlog["title"]}
+          description={dummyBlog["description"]}
+        />
+        <BlogCard
+          readTime={dummyBlog["readTime"]}
+          publishedDate={dummyBlog["publishedDate"]}
+          title={dummyBlog["title"]}
+          description={dummyBlog["description"]}
+        />
+      </div>
     </section>
   );
 }
